d8e48af2ce793d5857fa466ef5a79c9c042d7494
Nome Sobrenomemicael  git config --global user.email seu_email@email.comtecmidiainfor@gmail.com <tecmidiainfor@gmail.com>
2019-07-08 11:07:10 -0300

    alteracao php

--- valida.php
+++ valida.php
@@ -1,30 +1,26 @@
 <?php
+define('SERVER', 'mysql669.umbler.com');
+define('BANCO', 'atento');
+define('SENHA', 'atento2019');
+define('USER', 'micael');
 
-$host= 'mysql669.umbler.com:41890';
+$con = new pdo('mysql:host=' . SERVER . ';dbname=' . BANCO, USER, SENHA);
+
+try{
+
+$con = new pdo('mysql:host=' . SERVER . ';dbname=' . BANCO, USER, SENHA);
+
+}catch(PDOException $e){
+echo "Erro gerado " . $e->getMessage(); 
+}
+
+
+
+$host= 'mysql669.umbler.com';
 $bd= 'atento';
 $senhabd= 'micael';
 $userbd = 'atento2019';
 
 
 
-$nome = $_POST ["nome"]; 
-$idade = $_POST ["idade"];
-$estado = $_POST ["estado"]; 
-
-
-//Gravando no banco de dados !
-//conectando com o localhost - mysql
-$conexao = mysql_connect($host,$bd, $senhabd);
-if (!$conexao)
-die ("Erro de conexão com localhost, o seguinte erro ocorreu -> ".mysql_error());
-//conectando com a tabela do banco de dados
-$banco = mysql_select_db($bd,$conexao);
-if (!$banco)
-die ("Erro de conexão com banco de dados, o seguinte erro ocorreu -> ".mysql_error());
-$query = "INSERT INTO `clientes` ( `nome` , `email` , `sexo` , `ddd` , `telefone` , `endereço` , `cidade` , `estado` , `bairro` , `pa
-ís` , `login` , `senha` , `news` , `id` )
-VALUES ('$nome', '$email', '$sexo', '$ddd', '$tel', '$endereco', '$cidade', '$estado', '$bairro', '$pais', '$login', '$senha', '$news
-', '')";
-mysql_query($query,$conexao);
-echo "Seu cadastro foi realizado com sucesso!<br>Agradecemos a atenção.";
 ?> 
